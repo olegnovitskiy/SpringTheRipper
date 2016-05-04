da89c8c2517cf885d953a7aace38d8a3dcac784f
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-05-04 18:00:48 +0300

    Add BeanFactoryPostProcessor

---
+++ src/main/java/com/fmdev/quoter/DeprecationHandlerBeanFactoryPostProcessor.java
@@ -0,0 +1,11 @@
+package com.fmdev.quoter;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class DeprecationHandlerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+
+    }
+}
