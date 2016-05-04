1f3e93a5769c1452709a1f9044f7261f627b0a68
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-05-04 18:11:52 +0300

    DeprecationHandlerBeanFactoryPostProcessor implementation done!

--- src/main/java/com/fmdev/quoter/DeprecationHandlerBeanFactoryPostProcessor.java
+++ src/main/java/com/fmdev/quoter/DeprecationHandlerBeanFactoryPostProcessor.java
@@ -1,11 +1,27 @@
 package com.fmdev.quoter;
 
+import com.fmdev.quoter.annotation.DeprecatedClass;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 public class DeprecationHandlerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        final String[] names = beanFactory.getBeanDefinitionNames();
 
+        for (String name : names) {
+            final BeanDefinition beanDefinition = beanFactory.getBeanDefinition(name);
+            // Здесь можно вытащить только название класса
+            try {
+                final Class<?> beanClass = Class.forName(beanDefinition.getBeanClassName());
+                final DeprecatedClass annotation = beanClass.getAnnotation(DeprecatedClass.class);
+                if (annotation != null) {
+                    beanDefinition.setBeanClassName(annotation.newImpl().getName());
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
